f268b2f3cf988cd94b3751c29c496f5d1fb95ee1
Gabe Chino <gabe.chino@gmail.com>
2021-01-28 18:17:26 -0700

    latest

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-$str = "Mary Had A Little Lamb and She LOVED It So";
-$str = strtoupper($str);
-echo $str; // Prints MARY HAD A LITTLE LAMB AND SHE LOVED IT SO
+session_start();
+echo $_SESSION['SQLPW'];
 ?>
