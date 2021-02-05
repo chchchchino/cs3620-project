3a473e0d68779cfbc7711bbf021cd37e48e4ad2d
Gabe Chino <gabe.chino@gmail.com>
2021-02-04 21:24:08 -0700

    latest

--- utilities/connection.php
+++ utilities/connection.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $servername = "cs3620sql.mysql.database.azure.com";
 $username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 $password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
@@ -10,5 +9,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} 
 ?>
